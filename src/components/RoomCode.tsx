24e348570afe866c17c17fac31c40f0c18c5e52a
francisco-neto-fit <francisco.neto@fit-tecnologia.org.br>
2021-10-12 20:53:00 -0300

    adicionado a funcao de copiar o codigo e a opcao de adicionar comentario

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -1,12 +1,20 @@
 import copyImg from "../assets/images/copy.svg";
 import "../styles/room-code.scss";
-export function RoomCode() {
+
+type RoomCodeProps = {
+  code: string;
+};
+
+export function RoomCode(props: RoomCodeProps) {
+  function copyRoomCodeToClipBoard() {
+    navigator.clipboard.writeText(props.code);
+  }
   return (
-    <button className="room-code">
+    <button className="room-code" onClick={copyRoomCodeToClipBoard}>
       <div>
         <img src={copyImg} alt="Copy room code" />
       </div>
-      <span>Sala #9678</span>
+      <span>Sala #{props.code}</span>
     </button>
   );
 }
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,14 +1,49 @@
+import { FormEvent, useState } from "react";
+import { useParams } from "react-router-dom";
 import logoImg from "../assets/images/logo.svg";
 import { Button } from "../components/button";
 import { RoomCode } from "../components/RoomCode";
+import { useAuth } from "../hooks/useAuth";
+import { database } from "../services/firebase";
 import "../styles/room.scss";
+
+type RoomParams = {
+  id: string;
+};
 export function Room() {
+  const params = useParams<RoomParams>();
+  const [newQuestion, setNewQuestion] = useState("");
+  const roomId = params.id;
+  const { user } = useAuth(); // tem que estar autenticado
+
+  async function handleSendQuestion(event: FormEvent) {
+    // if e else = oeprador ternário (condicao) ? (se true) : (se false)
+    event.preventDefault(); // Pra n'ao recarregar a tela
+    if (newQuestion.trim() == "") {
+      return;
+    }
+    if (!user) {
+      //Se o usuário não tiver autenticado
+      throw new Error("You must be logged in");
+    }
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user.name,
+        avatar: user.avatar,
+      },
+      isHighLighted: false,
+      isAnswered: false,
+    };
+    await database.ref(`rooms/${roomId}/questions`).push(question); //Vai criar uma nova informação dentro do objeto rooms/roomsID
+    setNewQuestion("");
+  }
   return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="Letmeask" />
-          <RoomCode />
+          <RoomCode code={roomId} />
         </div>
       </header>
 
@@ -18,13 +53,26 @@ export function Room() {
           <span>4 Perguntas</span>
         </div>
 
-        <form>
-          <textarea placeholder="O que você quer perguntar?" />
+        <form onSubmit={handleSendQuestion}>
+          <textarea
+            placeholder="O que você quer perguntar?"
+            onChange={(event) => setNewQuestion(event.target.value)}
+            value={newQuestion}
+          />
           <div className="form-footer">
-            <span>
-              Para enviar uma pergunta, <button>faça seu login</button>.
-            </span>
-            <Button type="submit">Enviar Pergunta</Button>
+            {user ? (
+              <div className="user-info">
+                <img src={user.avatar} />
+                <span>{user.name}</span>
+              </div>
+            ) : (
+              <span>
+                Para enviar uma pergunta, <button>faça seu login</button>.
+              </span>
+            )}
+            <Button type="submit" disabled={!user}>
+              Enviar Pergunta
+            </Button>
           </div>
         </form>
       </main>
