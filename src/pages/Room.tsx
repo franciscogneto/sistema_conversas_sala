05a8a6541a798657a48efe84bc01f8852192efb1
francisco-neto-fit <francisco.neto@fit-tecnologia.org.br>
2021-10-13 16:41:44 -0300

    Adicionado informacoes na pagina da sala

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import logoImg from "../assets/images/logo.svg";
 import { Button } from "../components/button";
@@ -10,11 +10,65 @@ import "../styles/room.scss";
 type RoomParams = {
   id: string;
 };
+//Record == objeto
+type FirebaseQuestions = Record<
+  string,
+  {
+    author: {
+      name: string;
+      avatar: string;
+    };
+    content: string;
+    isAnswered: boolean;
+    isHighLighted: boolean;
+  }
+>;
+
+type Question = {
+  id: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  content: string;
+  isAnswered: boolean;
+  isHighLighted: boolean;
+};
+
 export function Room() {
   const params = useParams<RoomParams>();
   const [newQuestion, setNewQuestion] = useState("");
   const roomId = params.id;
   const { user } = useAuth(); // tem que estar autenticado
+  const [questions, setQuestion] = useState<Question[]>([]);
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    const roomRef = database.ref(`rooms/${roomId}`);
+    roomRef.once("value", (room) => {
+      //Esta ouvindo um evento dom firebase uma unica vez, para escutar mais de uma vez usa o `on`
+      //console.log(room.val()); // pega os valores da room
+      const databaseRoom = room.val();
+      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {}; // Para caso o objecto venha como null
+      console.log(firebaseQuestions);
+
+      //Cria um array com os objetos
+      const parsedQuestions = Object.entries(firebaseQuestions).map(
+        ([key, value]) => {
+          return {
+            id: key,
+            content: value.content,
+            author: value.author,
+            isHighLighted: value.isHighLighted,
+            isAnswered: value.isAnswered,
+          };
+        }
+      );
+      setQuestion(parsedQuestions);
+      setTitle(databaseRoom.title);
+    });
+  }, [roomId]); //DIspara evento sempre que algo mudar, o segundo parametro é um array de dependências, se dependias == vazio, executa apenas uma vez
+  //Toda vez que o roomId mudar, executa novamente o useEffect()
 
   async function handleSendQuestion(event: FormEvent) {
     // if e else = oeprador ternário (condicao) ? (se true) : (se false)
@@ -49,8 +103,8 @@ export function Room() {
 
       <main className="content">
         <div className="room-title">
-          <h1>Sala de React</h1>
-          <span>4 Perguntas</span>
+          <h1>Sala {title}</h1>
+          {questions.length > 0 && <span>{questions.length} Perguntas</span>}
         </div>
 
         <form onSubmit={handleSendQuestion}>
@@ -75,6 +129,7 @@ export function Room() {
             </Button>
           </div>
         </form>
+        {JSON.stringify(questions)}
       </main>
     </div>
   );
