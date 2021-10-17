0d4192d2ec219f5d4996f6c9264d39cdbd0e9060
francisco-neto-fit <francisco.neto@fit-tecnologia.org.br>
2021-10-17 20:52:33 -0300

    alteracoes de visibiluidade entre usuario e admin

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -133,18 +133,25 @@ export function AdminRoom() {
                 isAnswered={question.isAnswered}
                 isHighLighted={question.isHighLighted}
               >
-                <button
-                  type="button"
-                  onClick={() => handleCheckQuestionAsAnswered(question.id)}
-                >
-                  <img src={checkImg} alt="Marcar pergunta como respondida" />
-                </button>
-                <button
-                  type="button"
-                  onClick={() => handleHighLightQuestion(question.id)}
-                >
-                  <img src={answerImg} alt="Dar destaque a pergunta" />
-                </button>
+                {!question.isAnswered && (
+                  <>
+                    <button
+                      type="button"
+                      onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                    >
+                      <img
+                        src={checkImg}
+                        alt="Marcar pergunta como respondida"
+                      />
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() => handleHighLightQuestion(question.id)}
+                    >
+                      <img src={answerImg} alt="Dar destaque a pergunta" />
+                    </button>
+                  </>
+                )}
                 <button
                   type="button"
                   onClick={() => handleDeleteQuestion(question.id)}
